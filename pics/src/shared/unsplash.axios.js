e87374d577e698c5202637a2c6234fbeaa478205
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-03-22 20:18:41 +0800

    Commented console.log access key

--- pics/src/shared/unsplash.axios.js
+++ pics/src/shared/unsplash.axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const UNSPLASH_ACCESS_KEY = `${process.env.REACT_APP_UNSPLASH_CLIENT_ID}`;
-console.log(`UNSPLASH_ACCESS_KEY = ${UNSPLASH_ACCESS_KEY}`);
+// console.log(`UNSPLASH_ACCESS_KEY = ${UNSPLASH_ACCESS_KEY}`);
 const UNSPLASH_API_URL = `https://api.unsplash.com`;
 
 export default axios.create({
