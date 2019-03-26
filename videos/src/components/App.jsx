adba8819a6298d40ed123bceb38b2d4b32ca2563
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-03-26 10:24:48 +0800

    Added Video Detail component to display selected VideoItem's playback iFrame, title and description.

--- videos/src/components/App.jsx
+++ videos/src/components/App.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 import youtube from '../shared/youtube.axios';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const YOUTUBE_API_SEARCH_VIDEO_PATH = '/search';
+const DEFAULT_TERM = 'reactjs';
 
 class App extends Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    // Set default search to ensure the app showing default results
+    this.onTermSubmit(DEFAULT_TERM);
+  }
 
   onTermSubmit = async (term) => {
     // console.log(`this.state.term = ${term}`);
@@ -17,18 +24,18 @@ class App extends Component {
       }
     });
 
-    let videos = [];
     if (response && response.data && Array.isArray(response.data.items) && response.data.items.length > 0 ) {
-      videos = response.data.items;
+      this.setState({ 
+        videos: response.data.items, 
+        selectedVideo: response.data.items[0]
+      });
     }
 
-    this.setState({ videos });
-
-    console.log(`this.state.videos: \n`, this.state.videos);
+    // console.log(`this.state.videos: \n`, this.state.videos);
   }
 
   onVideoSelect = ( video ) =>{
-    console.log(`video: \n`, video);
+    console.log(`Selected video: \n`, video);
     this.setState({ selectedVideo: video });    
   }
 
@@ -36,7 +43,16 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={ this.onTermSubmit } />
-        <VideoList videos={ this.state.videos } onVideoSelect={ this.onVideoSelect } />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={ this.state.selectedVideo }/>
+            </div>
+            <div className="five wide column">
+              <VideoList videos={ this.state.videos } onVideoSelect={ this.onVideoSelect } />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ videos/src/components/VideoDetail.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const YOUTUBE_EMBED_URL = `https://www.youtube.com/embed`;
+
+const VideoDetail = ( {video} ) => {
+  // console.log(`video detail: \n`, video);
+  if (video === null) {
+    return <div>Please do searching and select a video.</div>
+  }
+
+  const videoSrc = `${YOUTUBE_EMBED_URL}/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={ videoSrc } title={ video.snippet.title } />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{ video.snippet.title }</h4>
+        <p>{ video.snippet.description }</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoDetail;
