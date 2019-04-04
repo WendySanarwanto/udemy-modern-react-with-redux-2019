1b0bab8bad0783092061b8e9de036c89c0f37ee4
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-04 12:43:39 +0800

    deploy

--- streams/src/components/App.jsx
+++ streams/src/components/App.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { 
+        //  HashRouter,
+        //  MemoryRouter,
+        BrowserRouter,
+        Route } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="ui container">Main Page !</div>
+      <div className="ui container" >
+        {/* <HashRouter> */}
+        {/* <MemoryRouter> */}
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate}/>
+            <Route path="/streams/edit" exact component={StreamEdit}/>
+            <Route path="/streams/delete" exact component={StreamDelete}/>
+            <Route path="/streams/show" exact component={StreamShow}/>
+          </div>
+        </BrowserRouter>
+        {/* </HashRouter> */}
+        {/* </MemoryRouter> */}
+      </div>
     );
   }
 }
