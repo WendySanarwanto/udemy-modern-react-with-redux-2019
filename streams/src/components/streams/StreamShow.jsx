e297b1bff0c42d464f4b9c04c9f8499a37026aa1
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-13 14:22:01 +0800

    Converted StreamShow as component

--- streams/src/components/streams/StreamShow.jsx
+++ streams/src/components/streams/StreamShow.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions/index';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchStream(id);
+  }
+
+  render() {
+    return (
+      <div>
+        StreamShow !
+      </div>
+    );
+  }
 }
 
-export default StreamShow;
+const mapStateToProps =  (state, ownProps) => {
+  const id = ownProps.match && ownProps.match.params ? ownProps.match.params.id : null;
+
+  return {
+    stream: state.streams[id]
+  };
+}
+
+export default connect(mapStateToProps, fetchStream)(StreamShow);
