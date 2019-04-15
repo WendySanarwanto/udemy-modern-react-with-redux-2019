9eb1ae905ac81446e5954705a1ce4481a4d7599a
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-15 19:24:47 +0800

    Added sample of Context.Consumer in Button component

--- translate/src/components/Button.jsx
+++ translate/src/components/Button.jsx
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 export default class Button extends Component {
-  static contextType = LanguageContext;
+  // When using Consumer, contextType should not be declared
+  // static contextType = LanguageContext; 
+  renderButtonLabel(value) {
+    return value === 'english' ? 'Submit' : 'Voorlegen';
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui button primary">{ text }</button>
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          { (language) => this.renderButtonLabel(language) }
+        </LanguageContext.Consumer>
+      </button>
     );
   }
 }
